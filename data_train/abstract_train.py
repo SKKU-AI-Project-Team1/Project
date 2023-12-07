3583385b96c0e7c399aed18b66daee965a60e767
Cloud User <ubuntu@aip.novalocal>
2023-12-08 00:12:37 +0900

    final file

--- data_train/abstract_train.py
+++ data_train/abstract_train.py
@@ -92,41 +92,22 @@ for train_index, val_index in skf.split(test, test['ipc_subclass_num']):
     test20_data=pd.concat([X_train, Y_train], axis=1)
     test_data = PatentDataset(test20_data, tokenizer_pretrained)
     break
-# # train, test 데이터셋 생성
-# train_data = PatentDataset(train, tokenizer_pretrained)
-# test_data = PatentDataset(test, tokenizer_pretrained)
+
 
 # DataLoader로 이전에 생성한 Dataset를 지정하여, batch 구성, shuffle, num_workers 등을 설정합니다.
 train_loader = DataLoader(train_data, batch_size=8, shuffle=True, num_workers=8)
 test_loader = DataLoader(test_data, batch_size=8, shuffle=True, num_workers=8)
-# train_loader과 test_loader는 iterator이다.
-# iterator란 반복을 이용해 어떤 처리를 수행하는 객체를 말한다.
-print('train_loader',train_loader)
-print('test_loader',test_loader)
 
-# 1개의 batch 꺼내기
+
 inputs, labels = next(iter(train_loader))
-
-# 데이터셋을 device 설정
 inputs = {k: v.to(device) for k, v in inputs.items()}
-
-
-
 config = BertConfig.from_pretrained(MODEL_NAME)
-
-
-# 모델 생성
 model = BertModel.from_pretrained(MODEL_NAME).to(device)
-
-
 output = model(**inputs)
-
 fc = nn.Linear(768, 2)
 fc.to(device)
 fc_output = fc(output['last_hidden_state'][:, 0, :])
-print(fc_output.shape)
-print(fc_output)
-print(fc_output.argmax(dim=1))
+
 
 class FineTuningBertModel(nn.Module):
     def __init__(self, bert_pretrained, dropout_rate=0.5):
@@ -171,98 +152,74 @@ def model_train(model, data_loader, loss_fn, optimizer, device):
     corr = 0
     counts = 0
 
-    # 예쁘게 Progress Bar를 출력하면서 훈련 상태를 모니터링 하기 위하여 tqdm으로 래핑합니다.
+
     prograss_bar = tqdm(data_loader, unit='batch', total=len(data_loader), mininterval=1)
 
-    # mini-batch 학습을 시작합니다.
+    # mini-batch 학습
     for idx, (inputs, labels) in enumerate(prograss_bar):
-        # inputs, label 데이터를 device 에 올립니다. (cuda:0 혹은 cpu)
+
         inputs = {k:v.to(device) for k, v in inputs.items()}
         labels = labels.to(device)
 
-        # 누적 Gradient를 초기화 합니다.
+
         optimizer.zero_grad()
 
-        # Forward Propagation을 진행하여 결과를 얻습니다.
         output = model(**inputs)
 
-        # 손실함수에 output, label 값을 대입하여 손실을 계산합니다.
         loss = loss_fn(output, labels)
 
-        # 오차역전파(Back Propagation)을 진행하여 미분 값을 계산합니다.
         loss.backward()
 
-        # 계산된 Gradient를 업데이트 합니다.
         optimizer.step()
 
-        # output의 max(dim=1)은 max probability와 max index를 반환합니다.
-        # max probability는 무시하고, max index는 pred에 저장하여 label 값과 대조하여 정확도를 도출합니다.
-        # print('output',output.shape) # (8, 2)
         _, index = output.max(dim=1)
 
-        # pred.eq(lbl).sum() 은 정확히 맞춘 label의 합계를 계산합니다. item()은 tensor에서 값을 추출합니다.
-        # 합계는 corr 변수에 누적합니다.
         corr += index.eq(labels).sum().item()
         counts += len(labels)
 
-        # loss 값은 1개 배치의 평균 손실(loss) 입니다. labels.size(0)은 배치사이즈(batch size) 입니다.
-        # loss 와 labels.size(0)를 곱하면 1개 배치의 전체 loss가 계산됩니다.
-        # 이를 누적한 뒤 Epoch 종료시 전체 데이터셋의 개수로 나누어 평균 loss를 산출합니다.
+
         running_loss += loss.item() * labels.size(0)
 
-        # 프로그레스바에 학습 상황 업데이트
-        prograss_bar.set_description(f"training loss: {running_loss/(idx+1):.5f}, training accuracy: {corr / counts:.5f}")
-        # 배치당 training loss,
 
-    # 누적된 정답수를 전체 개수로 나누어 주면 정확도가 산출됩니다.
+        prograss_bar.set_description(f"training loss: {running_loss/(idx+1):.5f}, training accuracy: {corr / counts:.5f}")
     acc = corr / len(data_loader.dataset)
 
-    # 평균 손실(loss)과 정확도를 반환합니다.
-    # train_loss, train_acc
+
     return running_loss / len(data_loader.dataset), acc
 
 def model_evaluate(model, data_loader, loss_fn, device):
-    # model.eval()은 모델을 평가모드로 설정을 바꾸어 줍니다.
-    # dropout과 같은 layer의 역할 변경을 위하여 evaluation 진행시 꼭 필요한 절차 입니다.
+
     model.eval()
 
-    # Gradient가 업데이트 되는 것을 방지 하기 위하여 반드시 필요합니다.
     with torch.no_grad():
-        # loss와 accuracy 계산을 위한 임시 변수 입니다. 0으로 초기화합니다.
+
         corr = 0
         running_loss = 0
 
-        # 배치별 evaluation을 진행합니다.
+        # evaluation진행
         for inputs, labels in data_loader:
-            # inputs, label 데이터를 device 에 올립니다. (cuda:0 혹은 cpu)
+
             inputs = {k:v.to(device) for k, v in inputs.items()}
             labels = labels.to(device)
 
-            # 모델에 Forward Propagation을 하여 결과를 도출합니다.
+
             output = model(**inputs) # (8,2)
 
-            # output의 max(dim=1)은 max probability와 max index를 반환합니다.
-            # max probability는 무시하고, max index는 pred에 저장하여 label 값과 대조하여 정확도를 도출합니다.
+
             _, index = output.max(dim=1)
 
-            # idx.eq(lbl).sum() 은 정확히 맞춘 label의 합계를 계산합니다. item()은 tensor에서 값을 추출합니다.
-            # 합계는 corr 변수에 누적합니다.
+
             corr += torch.sum(index.eq(labels)).item()
 
-            # loss 값은 1개 배치의 평균 손실(loss) 입니다. img.size(0)은 배치사이즈(batch size) 입니다.
-            # loss 와 img.size(0)를 곱하면 1개 배치의 전체 loss가 계산됩니다.
-            # 이를 누적한 뒤 Epoch 종료시 전체 데이터셋의 개수로 나누어 평균 loss를 산출합니다.
+
             running_loss += loss_fn(output, labels).item() * labels.size(0)
 
-        # validation 정확도를 계산합니다.
-        # 누적한 정답숫자를 전체 데이터셋의 숫자로 나누어 최종 accuracy를 산출합니다.
         acc = corr / len(data_loader.dataset)
 
-        # 결과를 반환합니다.
-        # val_loss, val_acc
+
         return running_loss / len(data_loader.dataset), acc
     
-# 최대 Epoch을 지정합니다.
+# 최대 Epoch
 num_epochs = 6
 
 # checkpoint로 저장할 모델의 이름을 정의 합니다.
--- data_train/base_train.py
+++ data_train/base_train.py
@@ -15,22 +15,20 @@ from transformers import AlbertModel
 
 print(f'torch version: {torch.__version__}')
 print(f'torchvision version: {torchvision.__version__}')
-# 토크나이저 관련 경고 무시하기 위하여 설정
 os.environ["TOKENIZERS_PARALLELISM"] = 'true'
 
-# device 지정
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f'사용 디바이스: {device}')
 
 
-train = pd.read_csv('../../data/sampled20_train_data.csv')
-test = pd.read_csv('../../data/valid_data.csv')
+train = pd.read_csv('../../data/train_data_v1.3.csv')
+test = pd.read_csv('../../data/valid_data_v1.3.csv')
 print('train',train.shape)
 print('test',test.shape)
 
 MODEL_NAME = 'kykim/bert-kor-base'
 
-# MODEL_NAME='beomi/KcELECTRA-base-v2022'
+
 
 
 
@@ -38,17 +36,17 @@ MODEL_NAME = 'kykim/bert-kor-base'
 class PatentDataset(Dataset):
 
     def __init__(self, dataframe, tokenizer_pretrained):
-        # sentence, label 컬럼으로 구성된 데이터프레임 전달
+
         self.data = dataframe
-        # Huggingface 토크나이저 생성
+
 
         self.tokenizer = BertTokenizerFast.from_pretrained(tokenizer_pretrained)
 
     def __len__(self):
         return len(self.data)
 
-    def __getitem__(self, idx): # 클래스의 인덱스에 접근할 때 자동으로 호출되는 메서드
-        # iloc : 행번호로 선택하는 방법
+    def __getitem__(self, idx): 
+
         sentence = self.data.iloc[idx]['invention_title']
         label = self.data.iloc[idx]['ipc_subclass_num']
 
@@ -61,14 +59,10 @@ class PatentDataset(Dataset):
             add_special_tokens=True  # 스페셜 토큰 적용
         )
 
-        input_ids = tokens['input_ids'].squeeze(0)       # 2D -> 1D : 1,512 -> 512
-        attention_mask = tokens['attention_mask'].squeeze(0) # 2D -> 1D
+        input_ids = tokens['input_ids'].squeeze(0)      
+        attention_mask = tokens['attention_mask'].squeeze(0) 
         token_type_ids = torch.zeros_like(attention_mask)
 
-        # input_ids, attention_mask, token_type_ids 이렇게 3가지 요소를 반환하도록 합니다.
-        # input_ids: 토큰
-        # attention_mask: 실제 단어가 존재하면 1, 패딩이면 0 (패딩은 0이 아닐 수 있습니다)
-        # token_type_ids: 문장을 구분하는 id. 단일 문장인 경우에는 전부 0
         return {
             'input_ids': input_ids,
             'attention_mask': attention_mask,
@@ -82,18 +76,17 @@ tokenizer_pretrained = MODEL_NAME
 train_data = PatentDataset(train, tokenizer_pretrained)
 test_data = PatentDataset(test, tokenizer_pretrained)
 
-# DataLoader로 이전에 생성한 Dataset를 지정하여, batch 구성, shuffle, num_workers 등을 설정합니다.
+
 train_loader = DataLoader(train_data, batch_size=8, shuffle=True, num_workers=8)
 test_loader = DataLoader(test_data, batch_size=8, shuffle=True, num_workers=8)
-# train_loader과 test_loader는 iterator이다.
-# iterator란 반복을 이용해 어떤 처리를 수행하는 객체를 말한다.
+
 print('train_loader',train_loader)
 print('test_loader',test_loader)
 
-# 1개의 batch 꺼내기
+
 inputs, labels = next(iter(train_loader))
 
-# 데이터셋을 device 설정
+
 inputs = {k: v.to(device) for k, v in inputs.items()}
 
 
@@ -101,7 +94,7 @@ inputs = {k: v.to(device) for k, v in inputs.items()}
 config = BertConfig.from_pretrained(MODEL_NAME)
 
 
-# 모델 생성
+
 model = BertModel.from_pretrained(MODEL_NAME).to(device)
 
 
@@ -116,132 +109,113 @@ print(fc_output.argmax(dim=1))
 
 class FineTuningBertModel(nn.Module):
     def __init__(self, bert_pretrained, dropout_rate=0.5):
-        # 부모클래스 초기화
+
         super().__init__()
-        # 사전학습 모델 지정
+
         self.bert = BertModel.from_pretrained(bert_pretrained)
 
-        # dropout 설정
+
         self.dropout = nn.Dropout(p=dropout_rate)
-        # 최종 출력층 정의
+
         self.fc = nn.Linear(768, 489)
 
     def forward(self, input_ids, attention_mask, token_type_ids):
-        # 입력을 pre-trained bert model 로 대입
+
         output = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-        # 결과의 last_hidden_state 가져옴
+
         last_hidden_state = output['last_hidden_state']
-        # last_hidden_state[:, 0, :]는 [CLS] 토큰을 가져옴
+
         x = self.dropout(last_hidden_state[:, 0, :])
-        # FC 을 거쳐 최종 출력
+
         x = self.fc(x)
         return x
     
 BERT_model = FineTuningBertModel(MODEL_NAME)
 BERT_model.to(device)
 
-# loss 정의: CrossEntropyLoss
+
 loss_fn = nn.CrossEntropyLoss()
 
-# 옵티마이저 정의: bert.paramters()와 learning_rate 설정
+
 optimizer = optim.Adam(BERT_model.parameters(), lr=1e-5)
 
-from tqdm import tqdm  # Progress Bar 출력
+from tqdm import tqdm 
 
 def model_train(model, data_loader, loss_fn, optimizer, device):
-    # 모델을 훈련모드로 설정합니다. training mode 일 때 Gradient 가 업데이트 됩니다. 반드시 train()으로 모드 변경을 해야 합니다.
+
     model.train()
 
-    # loss와 accuracy 계산을 위한 임시 변수 입니다. 0으로 초기화합니다.
     running_loss = 0
     corr = 0
     counts = 0
 
-    # 예쁘게 Progress Bar를 출력하면서 훈련 상태를 모니터링 하기 위하여 tqdm으로 래핑합니다.
+
     prograss_bar = tqdm(data_loader, unit='batch', total=len(data_loader), mininterval=1)
 
-    # mini-batch 학습을 시작합니다.
     for idx, (inputs, labels) in enumerate(prograss_bar):
-        # inputs, label 데이터를 device 에 올립니다. (cuda:0 혹은 cpu)
+
         inputs = {k:v.to(device) for k, v in inputs.items()}
         labels = labels.to(device)
 
-        # 누적 Gradient를 초기화 합니다.
         optimizer.zero_grad()
 
-        # Forward Propagation을 진행하여 결과를 얻습니다.
+
         output = model(**inputs)
 
-        # 손실함수에 output, label 값을 대입하여 손실을 계산합니다.
+
         loss = loss_fn(output, labels)
 
-        # 오차역전파(Back Propagation)을 진행하여 미분 값을 계산합니다.
+
         loss.backward()
 
-        # 계산된 Gradient를 업데이트 합니다.
+
         optimizer.step()
 
-        # output의 max(dim=1)은 max probability와 max index를 반환합니다.
-        # max probability는 무시하고, max index는 pred에 저장하여 label 값과 대조하여 정확도를 도출합니다.
-        # print('output',output.shape) # (8, 2)
+
         _, index = output.max(dim=1)
 
-        # pred.eq(lbl).sum() 은 정확히 맞춘 label의 합계를 계산합니다. item()은 tensor에서 값을 추출합니다.
-        # 합계는 corr 변수에 누적합니다.
+
         corr += index.eq(labels).sum().item()
         counts += len(labels)
 
-        # loss 값은 1개 배치의 평균 손실(loss) 입니다. labels.size(0)은 배치사이즈(batch size) 입니다.
-        # loss 와 labels.size(0)를 곱하면 1개 배치의 전체 loss가 계산됩니다.
-        # 이를 누적한 뒤 Epoch 종료시 전체 데이터셋의 개수로 나누어 평균 loss를 산출합니다.
+
         running_loss += loss.item() * labels.size(0)
 
-        # 프로그레스바에 학습 상황 업데이트
-        prograss_bar.set_description(f"training loss: {running_loss/(idx+1):.5f}, training accuracy: {corr / counts:.5f}")
-        # 배치당 training loss,
 
-    # 누적된 정답수를 전체 개수로 나누어 주면 정확도가 산출됩니다.
+        prograss_bar.set_description(f"training loss: {running_loss/(idx+1):.5f}, training accuracy: {corr / counts:.5f}")
+
+
     acc = corr / len(data_loader.dataset)
 
-    # 평균 손실(loss)과 정확도를 반환합니다.
-    # train_loss, train_acc
+
     return running_loss / len(data_loader.dataset), acc
 
 def model_evaluate(model, data_loader, loss_fn, device):
-    # model.eval()은 모델을 평가모드로 설정을 바꾸어 줍니다.
-    # dropout과 같은 layer의 역할 변경을 위하여 evaluation 진행시 꼭 필요한 절차 입니다.
+
     model.eval()
 
-    # Gradient가 업데이트 되는 것을 방지 하기 위하여 반드시 필요합니다.
     with torch.no_grad():
-        # loss와 accuracy 계산을 위한 임시 변수 입니다. 0으로 초기화합니다.
+
         corr = 0
         running_loss = 0
 
         # 배치별 evaluation을 진행합니다.
         for inputs, labels in data_loader:
-            # inputs, label 데이터를 device 에 올립니다. (cuda:0 혹은 cpu)
+
             inputs = {k:v.to(device) for k, v in inputs.items()}
             labels = labels.to(device)
 
-            # 모델에 Forward Propagation을 하여 결과를 도출합니다.
-            output = model(**inputs) # (8,2)
 
-            # output의 max(dim=1)은 max probability와 max index를 반환합니다.
-            # max probability는 무시하고, max index는 pred에 저장하여 label 값과 대조하여 정확도를 도출합니다.
+            output = model(**inputs) 
+
             _, index = output.max(dim=1)
 
-            # idx.eq(lbl).sum() 은 정확히 맞춘 label의 합계를 계산합니다. item()은 tensor에서 값을 추출합니다.
-            # 합계는 corr 변수에 누적합니다.
             corr += torch.sum(index.eq(labels)).item()
 
-            # loss 값은 1개 배치의 평균 손실(loss) 입니다. img.size(0)은 배치사이즈(batch size) 입니다.
-            # loss 와 img.size(0)를 곱하면 1개 배치의 전체 loss가 계산됩니다.
-            # 이를 누적한 뒤 Epoch 종료시 전체 데이터셋의 개수로 나누어 평균 loss를 산출합니다.
+
             running_loss += loss_fn(output, labels).item() * labels.size(0)
 
-        # validation 정확도를 계산합니다.
-        # 누적한 정답숫자를 전체 데이터셋의 숫자로 나누어 최종 accuracy를 산출합니다.
+        # validation 정확도 계산
         acc = corr / len(data_loader.dataset)
 
         # 결과를 반환합니다.
